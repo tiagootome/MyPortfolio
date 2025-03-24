fd84f8e1776781b3666c11178836351bec6f5d92
Tiago Tomé <tiagotome097@gmail.com>
2025-03-24 18:13:50 -0300

    update link github

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,63 +1,65 @@
 // React
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 // CSS
-import "./Home.css"
-import "./HomeResponsivo.css"
+import "./Home.css";
+import "./HomeResponsivo.css";
 
 // Imagens
-import perfil from "../assets/IMG_0910.jpg"
-import html from "../assets/html.png"
-import css from "../assets/css.png"
-import js from "../assets/js.png"
-import react from "../assets/react.png"
-import github from "../assets/github.png"
-import linkedin from "../assets/linkedin.png"
-import insta from "../assets/instagram.png"
-import logo from "../assets/logo.png"
+import perfil from "../assets/IMG_0910.JPG";
+import html from "../assets/html.png";
+import css from "../assets/css.png";
+import js from "../assets/js.png";
+import react from "../assets/react.png";
+import github from "../assets/github.png";
+import linkedin from "../assets/linkedin.png";
+import insta from "../assets/instagram.png";
+import logo from "../assets/logo.png";
 
 const Home = () => {
+  const [projetos, setProjetos] = useState([]);
+  const [idade, setIdade] = useState(0);
+  const [anoAtual, setAnoAtual] = useState(new Date().getFullYear());
 
-    const [projetos, setProjetos] = useState([]);
-    const [idade, setIdade] = useState(0);
-    const [anoAtual, setAnoAtual] = useState(new Date().getFullYear());
-      
-    useEffect(() => {
-      const fetchProjetos = async () => {
-        try {
-          const response = await fetch('https://api.github.com/users/tiagoolima/repos');
-          const data = await response.json();
-          setProjetos(data);
-        } catch (error) {
-          console.error('Erro ao obter os projetos do GitHub:', error);
-        }
-      };
-  
-      fetchProjetos();
+  useEffect(() => {
+    const fetchProjetos = async () => {
+      try {
+        const response = await fetch(
+          "https://api.github.com/users/tiagootome/repos"
+        );
+        const data = await response.json();
+        console.log("Dados recebidos da API:", data);
+        setProjetos(data);
+      } catch (error) {
+        console.error("Erro ao obter os projetos do GitHub:", error);
+      }
+    };
 
-      const dataNascimentoString = "1997-12-17"
-      const dataNascimento = new Date(dataNascimentoString)
-      const hoje = new Date()
-      const diferencaAnos = hoje.getFullYear() - dataNascimento.getFullYear();
+    fetchProjetos();
 
-      const aniversarioAconteceu = 
-        hoje.getMonth() > dataNascimento.getMonth() || 
-        (hoje.getMonth() === dataNascimento.getMonth() && hoje.getDate() >= dataNascimento.getDate());
-        
-      const idadeAtual = aniversarioAconteceu ? diferencaAnos : diferencaAnos - 1;
+    const dataNascimentoString = "1997-12-17";
+    const dataNascimento = new Date(dataNascimentoString);
+    const hoje = new Date();
+    const diferencaAnos = hoje.getFullYear() - dataNascimento.getFullYear();
 
-      setIdade(idadeAtual);
+    const aniversarioAconteceu =
+      hoje.getMonth() > dataNascimento.getMonth() ||
+      (hoje.getMonth() === dataNascimento.getMonth() &&
+        hoje.getDate() >= dataNascimento.getDate());
 
+    const idadeAtual = aniversarioAconteceu ? diferencaAnos : diferencaAnos - 1;
 
-      const intervalId = setInterval(() => {
-        const hoje = new Date();
-        if (hoje.getMonth() === 0 && hoje.getDate() === 1) {
-          setAnoAtual(hoje.getFullYear());
-        }
-      }, 60000); 
+    setIdade(idadeAtual);
 
-      return () => clearInterval(intervalId);
-    }, []);
+    const intervalId = setInterval(() => {
+      const hoje = new Date();
+      if (hoje.getMonth() === 0 && hoje.getDate() === 1) {
+        setAnoAtual(hoje.getFullYear());
+      }
+    }, 60000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <>
@@ -68,9 +70,15 @@ const Home = () => {
         <div class="menu">
           <nav>
             <ul>
-              <li><a href="#projetos">meus projetos</a></li>
-              <li><a href="#sobre">sobre mim</a></li>
-              <li><a href="#minhasHabilidades">minhas habilidades</a></li>
+              <li>
+                <a href="#projetos">meus projetos</a>
+              </li>
+              <li>
+                <a href="#sobre">sobre mim</a>
+              </li>
+              <li>
+                <a href="#minhasHabilidades">minhas habilidades</a>
+              </li>
             </ul>
           </nav>
         </div>
@@ -82,13 +90,40 @@ const Home = () => {
           </div>
 
           <div className="saudacao">
-            <h1>Olá! Eu sou o <br/><span>Tiago Tomé</span></h1>
-            <p>Sou um desenvolvedor <span>Front-End</span>.</p>
+            <h1>
+              Olá! Eu sou o <br />
+              <span>Tiago Tomé</span>
+            </h1>
+            <p>
+              Sou um desenvolvedor <span>Front-End</span>.
+            </p>
 
             <div className="redesSociais">
-              <a href="https://github.com/tiagootome" target="_blank" rel="noopener noreferrer"><img className="logoRedesSociais" src={github} alt="GitHub"/></a>
-              <a href="https://www.instagram.com/tiagootome/" target="_blank" rel="noopener noreferrer"><img className="logoRedesSociais" src={insta} alt="Instagram"/></a>
-              <a href="https://www.linkedin.com/in/tiagootome/" target="_blank" rel="noopener noreferrer"><img className="logoRedesSociais" src={linkedin} alt="Linkedin"/></a>
+              <a
+                href="https://github.com/tiagootome"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img className="logoRedesSociais" src={github} alt="GitHub" />
+              </a>
+              <a
+                href="https://www.instagram.com/tiagootome/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img className="logoRedesSociais" src={insta} alt="Instagram" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/tiagootome/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="logoRedesSociais"
+                  src={linkedin}
+                  alt="Linkedin"
+                />
+              </a>
             </div>
           </div>
         </section>
@@ -98,15 +133,36 @@ const Home = () => {
           <ul>
             {projetos.map((projeto) => (
               <li key={projeto.id}>
-                <a href={projeto.html_url} target="_blank" rel="noopener noreferrer">{projeto.name}</a>
+                <a
+                  href={projeto.html_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {projeto.name}
+                </a>
               </li>
             ))}
-            </ul>
+          </ul>
         </section>
 
         <section className="sobreMim" id="sobre">
           <h1 className="titulosSecoes">Sobre mim</h1>
-          <p>Meu nome é <span>Tiago Tomé</span>, tenho {idade} anos e moro em Iguatu / Ceará. Apaixonado por ciências exatas e tecnologia, sou graduado em <span>Matemática</span> pelo Instituto Federal de Educação, Ciência e Tecnologia do Ceará - <i>Campus</i> Cedro, onde desenvolvi habilidades analíticas e de resolução de problemas. Também sou <span>Técnico de Informática</span> pelo Instituto Federal de Educação, Ciência e Tecnologia do Ceará - <i>Campus</i> Iguatu, onde adquiri conhecimentos práticos em sistemas computacionais. <br/> Atualmente estou cursando <span>Análise e Desenvolvimento de Sistemas</span>, com foco em <span>Desenvolvimento Web Front-End</span>. Minha paixão pela tecnologia me faz buscar sempre aprender novas linguagens de programação e ferramentas para criar experiências digitais de alta qualidade.</p>
+          <p>
+            Meu nome é <span>Tiago Tomé</span>, tenho {idade} anos e moro em
+            Iguatu / Ceará. Apaixonado por ciências exatas e tecnologia, sou
+            graduado em <span>Matemática</span> pelo Instituto Federal de
+            Educação, Ciência e Tecnologia do Ceará - <i>Campus</i> Cedro, onde
+            desenvolvi habilidades analíticas e de resolução de problemas.
+            Também sou <span>Técnico de Informática</span> pelo Instituto
+            Federal de Educação, Ciência e Tecnologia do Ceará - <i>Campus</i>{" "}
+            Iguatu, onde adquiri conhecimentos práticos em sistemas
+            computacionais. <br /> Atualmente estou cursando{" "}
+            <span>Análise e Desenvolvimento de Sistemas</span>, com foco em{" "}
+            <span>Desenvolvimento Web Front-End</span>. Minha paixão pela
+            tecnologia me faz buscar sempre aprender novas linguagens de
+            programação e ferramentas para criar experiências digitais de alta
+            qualidade.
+          </p>
         </section>
 
         <section className="habilidades" id="minhasHabilidades">
@@ -116,13 +172,13 @@ const Home = () => {
           <img className="logoTecnologias" src={js} alt="" />
           <img className="logoTecnologias" src={react} alt="" />
         </section>
-        </main>
+      </main>
 
-        <footer>
-          <p>&copy; {anoAtual} Tiago Tomé.</p>
-        </footer>
+      <footer>
+        <p>&copy; {anoAtual} Tiago Tomé.</p>
+      </footer>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
